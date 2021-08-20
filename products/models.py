ce31c683f76675bd845dc9847227f411f7120f55
shm <thsrns3934@gmail.com>
2021-08-20 20:43:10 +0900

    Add: 검색 기능 추가

--- products/models.py
+++ products/models.py
@@ -3,22 +3,22 @@ from django.db import models
 from users.models import User
 
 class Origin(models.Model):
-    class OriginStatus(models.IntegerChoices):
+    class Type(models.IntegerChoices):
         DOMESTIC = 1
         IMPORTED = 2
 
-    status = models.IntegerField(choices=OriginStatus.choices)
+    name = models.CharField(max_length=20, choices=Type.choices)
 
     class Meta:
         db_table = 'origins'
 
 class Storage(models.Model):
-    class StorageStatus(models.IntegerChoices):
+    class Type(models.IntegerChoices):
         COLD   = 1
         FROZEN = 2
         DRY    = 3
 
-    status = models.IntegerField(choices=StorageStatus.choices)
+    name = models.CharField(max_length=20, choices=Type.choices)
 
     class Meta:
         db_table = 'storages'
@@ -37,16 +37,16 @@ class Product(models.Model):
         db_table = 'products'
 
 class Image(models.Model):
-    product = models.ForeignKey("Product", on_delete=models.CASCADE)
-    url = models.URLField(max_length=500)
+    product      = models.ForeignKey("Product", on_delete=models.CASCADE)
+    url          = models.URLField(max_length=500)
     is_thumbnail = models.BooleanField(null=True, default=False)
 
     class Meta:
         db_table = 'images'
 
 class Order(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    product = models.ForeignKey("Product", on_delete=models.CASCADE)
+    user     = models.ForeignKey(User, on_delete=models.CASCADE)
+    product  = models.ForeignKey("Product", on_delete=models.CASCADE)
     quantity = models.IntegerField()
 
     class Meta:
