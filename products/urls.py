59e0f6322227460ca00d21b6a8c011f069924bfa
shm <thsrns3934@gmail.com>
2021-08-20 20:43:10 +0900

    Add: 검색 기능 추가

--- products/urls.py
+++ products/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
 
+from products.views import SearchView
+
 urlpatterns = [
+        path('/search', SearchView.as_view())
 ]
--- products/views.py
+++ products/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.http  import JsonResponse
+from django.views import View
+from django.db.models import Case, When
+
+from users.models import User
+from products.models import Origin, Storage, Product, Image
+
+class SearchView(View):
+    def get(self, request):
+        keyword  = request.GET.get("keyword", "")
+
+        if not keyword:
+            return JsonResponse({"seller": [], "item": []}, status=200)
+
+        users    = User.objects.filter(name__icontains=keyword)
+        products = Product.objects.filter(name__icontains=keyword).annotate(thumbnail=Case(When(image__is_thumbnail=True, then='image__url'))).exclude(thumbnail=None)
+
+        seller = [{
+            "id"            : user.id,
+            "kakao_account" : user.kakao_account,
+            "name"          : user.name,
+            "profile_image" : user.profile_image,
+            } for user in users]
+
+        item = [{
+            "id"       : product.id,
+            "name"     : product.name,
+            "price"    : product.price,
+            "stock"    : product.stock,
+            "image"    : product.thumbnail
+        } for product in products]
+
+        return JsonResponse({"seller": seller, "item": item}, status=200)
