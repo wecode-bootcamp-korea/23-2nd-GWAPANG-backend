0135b9358d9b6d79b877fccae8b4847913cc1405
preferbaik <baik9261@gmail.com>
2021-08-20 14:48:17 +0900

    KaKao social login
    KAKAO Login

--- users/models.py
+++ users/models.py
@@ -2,9 +2,11 @@ from django.db import models
 
 class User(models.Model):
     kakao_account = models.CharField(max_length=100)
-    point         = models.IntegerField()
+    point         = models.IntegerField(default=100000)
     name          = models.CharField(max_length=100)
-    profile_image = models.URLField(max_length=500, null=True)
+    profile_image = models.URLField(max_length=500)
+    email         = models.CharField(max_length=200, null=True)
+
 
     class Meta:
         db_table = 'users'
--- users/views.py
+++ users/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+import jwt, requests, random
 
-# Create your views here.
+from django.views         import View
+from django.http.response import JsonResponse
+
+from my_settings          import SECRET_KEY, ALGORITHM
+from users.models         import User
+
+
+class KakaoLoginView(View):
+    def get(self, request):
+        try:
+            access_token = request.headers['Authorization']
+            url          = "https://kapi.kakao.com/v2/user/me"
+            header       = {
+                'Authorization' : 'Bearer {}'.format(access_token)
+            }
+            response = requests.get(url, headers = header).json()                
+
+            user, is_user = User.objects.get_or_create(
+                kakao_account     = response['id'], 
+                email             = response['kakao_account']['email'], 
+                name              = response['kakao_account']['profile']['nickname'],
+                profile_image_url = response['kakao_account']['profile']['profile_image_url'],
+            )
+            if is_user:
+                token = jwt.encode({'id': user.id}, SECRET_KEY, algorithm=ALGORITHM)
+                return JsonResponse({'MESSAGE': 'SUCCESS', 'user_name': user.name, 'TOKEN': token}, status = 200)
+            else:
+                token = jwt.encode({'id': user.id}, SECRET_KEY, algorithm=ALGORITHM)
+                return JsonResponse({'MESSAGE': 'SUCCESS', 'user_name': user.name, 'TOKEN': token}, status = 201)
+
+        except KeyError:
+            return JsonResponse({'message': 'KEY_ERROR'}, status=400)
