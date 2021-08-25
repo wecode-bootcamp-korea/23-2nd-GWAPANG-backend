fbccc5f9cdf0b19d274a2c118e0d0f83e0a46bdd
preferbaik <baik9261@gmail.com>
2021-08-25 21:14:47 +0900

    detailpage

--- products/urls.py
+++ products/urls.py
@@ -1,10 +1,10 @@
 from django.urls    import path
-
-from products.views import ProductView, SearchView, SellerListView, SellerProductsView
+from products.views import ProductView, SearchView, SellerListView, SellerProductsView, DetailPageView
 
 urlpatterns = [
     path('/search', SearchView.as_view()),
     path('/seller', SellerListView.as_view()),
     path('/seller/<user_id>', SellerProductsView.as_view()),
     path("", ProductView.as_view()),
+    path("/<int:product_id>", DetailPageView.as_view())
 ]
