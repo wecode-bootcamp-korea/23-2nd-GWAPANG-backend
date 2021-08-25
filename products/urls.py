a638d57ea20887f11c7d0356ce93d6a77ba7d51b
preferbaik <baik9261@gmail.com>
2021-08-25 14:54:36 +0900

    upload & update

--- products/urls.py
+++ products/urls.py
@@ -1,9 +1,8 @@
 from django.urls    import path
-
-from products.views import UploadProductView, SearchView, SellerListView
+from products.views import ProductView, SearchView, SellerListView
 
 urlpatterns = [
     path('/search', SearchView.as_view()),
     path('/seller', SellerListView.as_view()),
-    path("/upload", UploadProductView.as_view()),
+    path("", ProductView.as_view()),
 ]
